fb0fec2f9019ccc30f9c0b6b480f7fbe8a091057
shruthi093 <jayasruthi.l@gmail.com>
2021-12-23 11:40:50 +0530

    js

---
+++ js/basics.js
@@ -0,0 +1,52 @@
+
+//number
+//string
+//boolean
+//big int
+//array
+/*let a=false
+console.log(a)
+console.log(typeof(a))*/
+/*let student_names=["john",2345,false]
+console.log(student_names[0])
+console.log(student_names[1])
+console.log(student_names[2])
+console.log(typeof student_names)
+let student_details=[
+    {
+        fname:"peter",
+        email:"peter@gmail.com",
+        phone:"9999999",
+    },
+    {
+        fname:"george",
+        email:"george@gmail.com",
+        phone:"9999999",
+    },
+    {
+        fname:"henry",
+        email:"henry@gmail.com",
+        phone:"9999999",
+    }
+]// array of object
+
+let staff={
+        fname:"staffid01",
+        email:"staff@gmail.com",
+        phone:"9999999",
+        subjects:["MERN","java"]
+}// array inside obj
+console.log(student_details[0].email)
+console.log(staff.subjects[0])
+// arithmetic  + -  % */
+
+/*let a=10
+let b=20
+let c=a%b
+console.log(c)*/
+let a=12
+let b=13
+let c= a++ + b++
+console.log(a)
+console.log(b)
+console.log(c)

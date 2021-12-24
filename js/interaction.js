25b2d4bd47edc5bcc1dc30d20fb553f39a13ec9a
shruthi093 <jayasruthi.l@gmail.com>
2021-12-24 11:34:34 +0530

    operators and interaction

---
+++ js/interaction.js
@@ -0,0 +1,45 @@
+/*let uname="john"
+
+
+alert("Username : "+uname)*/
+/*let age=10
+let modified_age=Boolean(age)
+console.log(modified_age)
+console.log(typeof modified_age)*/
+/*let no_of_arrears=Number(prompt("enter your age",0))
+alert(`you have ${no_of_arrears} arrears`)*/
+/*let option=confirm("do you want to proceed?")
+console.log(option)*/
+//comparison operators
+// == -> check the value alone
+// === -> check the value and D.T
+//!= -> not equals
+//!== -> checks the value and D.T
+//console.log(2!=="2")
+//console.log("BEE">"BE")
+
+/*let a=null
+//console.log(typeof a)
+let b=0;
+//console.log(typeof b)
+//console.log(a===b)
+console.log(a>b)// false 
+console.log(a>=b)//true*/
+/*let a=60
+let b=70
+let c=40
+console.log(!(a>=b)&&a>=c)*/
+/*let b=0
+if(b){
+    alert("eligible for vaccination")
+}
+else{
+    alert("not eligible")
+}*/
+const PI=3.14
+console.log(PI)
+
+
+
+
+
